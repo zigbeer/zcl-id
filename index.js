5f6a99f73a6bb098020a903f3e858de76b194877
Simen Li <simenkid@gmail.com>
2016-05-13 23:18:15 +0800

    api interface

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
+var clusterId,
+    clusterAttrs;
+
 var zcl = {};
 
-zcl.clusterId = function (cId) {};
-zcl.attrId = function (cId, attrId) {};
-zcl.dataType = function (type) {};
-zcl.profId = function (prof) {};
-zcl.foundationCmd = function (cmd) {};
-zcl.deviceId = function (prof, devId) {};
-zcl.direction = function (dir) {};
-zcl.acl = function (acl) {};
-zcl.cmd = function (cId, cmdId) {};
-zcl.cmdRsp = function (cId, rspId) {};
-zcl.commands = function (cId) {};
+zcl.clusterId = function (cId) {
+    var item = clusterId.get(cId);
+};
+
+zcl.attrId = function (cId, attrId) {
+    var clusterAttr = clusterAttrs[cId];
+
+    if (clusterAttr)
+        return clusterAttr.get(attrId);
+};
+
+zcl.dataType = function (type) {
+    return dataType.get(type);
+};
+
+zcl.profId = function (prof) {
+    return profId.get(prof);
+};
+
+zcl.foundationCmd = function (cmd) {
+    return fcmd.get(cmd);
+};
+
+zcl.deviceId = function (prof, devId) {
+    var profIdStr = zcl.profId(prof),
+        dId;
+
+    if (profIdStr)
+        profIdStr = profIdStr.key;
+     
+    dId = deviceId[prof];
+
+    if (dId)
+        return dId.get(devId);
+};
+
+zcl.direction = function (dir) {
+    return direction.get(dir);
+};
+
+zcl.acl = function (acl) {
+    return acl.get(acl);
+};
+
+zcl.cmd = function (cId, cmdId) {
+
+};
+
+zcl.cmdRsp = function (cId, rspId) {
+
+};
+
+zcl.commands = function (cId) {
+    
+};
+
+module.exports = zcl;
